e52575ae0854c56c44b10e5359961caffb00a166
vijayalakshmi1204 <vijayalakshmimurali877@gmail.com>
2025-12-24 20:37:22 +0530

    Add files via upload

---
+++ even numbers.py
@@ -0,0 +1,5 @@
+n = int(input("Enter the limit: "))
+
+print("Even numbers are:")
+for i in range(2, n + 1, 2):
+    print(i, end=" ")
---
+++ sum of  two numbers.py
@@ -0,0 +1,7 @@
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+sum = a + b
+
+print("Sum of the two numbers is:", sum)
