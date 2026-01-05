97d8575dbd095d192815a50b68f92e62cd80e330
vijayalakshmi1204 <vijayalakshmimurali877@gmail.com>
2026-01-05 21:01:37 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,4 @@
+my_dict = {'a': 10, 'b': 20, 'c': 30}
+
+total = sum(my_dict.values())
+print("Sum of all values:", total)
---
+++ tuple.py
@@ -0,0 +1,5 @@
+my_list = [1, 2, 3]
+my_tuple = (4, 5)
+
+my_list.append(my_tuple)
+print(my_list)
